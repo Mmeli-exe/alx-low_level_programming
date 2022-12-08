9d169c280efaff958a3cf766d19b07e74a69405a
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-08 15:57:31 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function prints text
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	char outStr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", outStr);
+	return (0);
+}
