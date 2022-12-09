d6116fc82d3e229d9b76963e27dfdd39ff7ebe67
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-09 15:48:11 -0800

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char  num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
