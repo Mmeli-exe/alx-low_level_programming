29947bbdad1afbee8a2ea9a1f2b4974e2b305d7d
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-09 15:30:43 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char  ltr;
+	
+	for (ltr = a; ltr <= z; ltr++)
+	{
+		putchar(ltr);
+	}
+	putchar('\n');
+	return (0);
+
+}
