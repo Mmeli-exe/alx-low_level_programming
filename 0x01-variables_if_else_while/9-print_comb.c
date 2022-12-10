9da6b768ae6e401cffe17fad22988ff611662693
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-09 16:29:55 -0800

    list

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the single digit numbers
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char  num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + 48);
+		if (num < 9)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+	}
+	putchar("\n");
+	return (0);
+}
