640cb7e7efd030bab889a483a582afb3479bad1b
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-09 17:05:30 -0800

    htu

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the double digit numbers
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	int hunds;
+	int tens;
+	int units;
+	for (hunds = 0; hunds < 10; hunds++)
+	{
+		for (tens = hunds + 1; tens < 10; tens++)
+		{
+			for (units = tens + 1; units < 10; units++)
+			{
+				putchar(tens + 48);
+				putchar(units + 48);
+				if (units != 9 || tens != 8 || hunds != 7)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
