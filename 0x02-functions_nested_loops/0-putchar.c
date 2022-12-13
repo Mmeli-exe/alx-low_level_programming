5d5feddcfbc8b6b61a687be952b5a22c4f23009f
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-13 15:30:38 -0800

    betty cleared

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,12 @@
  * Return: 0 always success
  */
 
-int print_putchar()
+int print_putchar(void)
 {
 	int ltr;
 	char str[] = "_putchar";
 
 	for (ltr = 0; ltr < 8; ltr++)
 		_putchar(str[ltr]);
+	return (0);
 }
