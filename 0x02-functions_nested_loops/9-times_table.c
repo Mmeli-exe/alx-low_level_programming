6167aeefb1f84dd04ff2419812e5a9893ee1edba
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-13 18:00:03 -0800

    edit tt

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,23 @@ void times_table(void)
 {
 	int r;
 	int c;
+	int p;
 
 	for (r = 0; r < 10; r++)
 	{
 		for (c = 0; c < 10; c++)
 		{
-			_putchar((r*c) + 48);
-			if (c !=9)
+			p = r * c;
+			if (p < 10)
+			{
+				_putchar((r * c) + 48);
+			}
+			else
+			{
+				_putchar((p / 10) + 48);
+				_putchar((p % 10) + 48);
+			}
+			if (c != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
