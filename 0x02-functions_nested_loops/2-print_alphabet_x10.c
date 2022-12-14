2f08daecbc081fb23cfae89690bc6f947fd66515
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-13 16:12:33 -0800

    10x in just one call

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times
+ *Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int lns;
+
+	for (lns = 0; lns < 10; lns++)
+		print_alphabet();
+}
