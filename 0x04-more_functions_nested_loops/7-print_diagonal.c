d9bb8c3d4e889a683c4e4118c8734b4a59cb0b37
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-15 19:00:48 -0800

    diagon alley

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: number of \ to be printed
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int slsh;
+	int spc
+
+	for (slsh = 0; slsh < n; slsh++)
+	{
+		for (spc = 0; spc < slsh + 1; spc++)
+			_putchar(' ');
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
+
