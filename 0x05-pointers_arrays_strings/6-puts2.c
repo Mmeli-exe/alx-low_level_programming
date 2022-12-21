ea7d055f486d24b91f03ca4aa26a3436afb37e75
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-21 06:48:13 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ * @str: string source pointer
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int idx = 0;
+
+	while (str[idx] != '\0')
+	{
+		if (idx % 2 == 0)
+			_putchar(s[idx]);
+		idx++;
+	}
+}
