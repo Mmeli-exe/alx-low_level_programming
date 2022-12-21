077adb041a0abf5091a91616caa065343f6d24b2
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-21 07:17:55 -0800

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * print_array - print n values of an array
+ * @a: source array
+ * @n: number of values for be printed
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int idx;
+
+	for (idx = 0; idx < n; idx++)
+	{
+		printf("%d", a[idx]);
+		if ((idx + 1) < n)
+			printf(", ");
+	}
+	printf("\n");
+}
