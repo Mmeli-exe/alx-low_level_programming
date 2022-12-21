ada61da3f7ee6b77e20746662da2fbddf3ee3378
Mmeli Mpondo <mmelimpondo@gmail.com>
2022-12-21 07:40:04 -0800

    cpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy src to dest
+ * @dest: destination char array
+ * @src: source string literal
+ * Return: dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int idx = 0;
+
+	do {
+		dest[idx] = src[idx];
+		idx++;
+	} while (src[idx - 1] != 0)
+	return (*dest);
+}
